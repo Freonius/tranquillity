68abe1661130ebf41ae0fe303057f87f3a47ba7c
Federico Pirani <freonius@gmail.com>
2021-10-30 21:52:44 +0200

    Couchdb and settings

--- src/tranquillity/connections/_couchdb.py
+++ src/tranquillity/connections/_couchdb.py
@@ -1,11 +1,14 @@
 from typing import Union
-from couchdb import Server, Session, ServerError
+from couchdb import Server, Session, ServerError, Unauthorized, ResourceNotFound
 from couchdb.client import Database
 from ..interfaces import IConnection
+from ..exceptions import ConnectionException
 
 class CouchDb(IConnection):
     _client: Union[Server, None] = None
     _session: Session = Session()
+    _token: Union[str, None] = None
+    _db: Union[Database, None] = None
 
     def connect(self) -> None:
         _host: str = self._settings['couchdb.host']
@@ -26,19 +29,49 @@ class CouchDb(IConnection):
         except KeyError:
             pass
         try:
+            _url: str = f'{_protocol}://{_host}:{_port}'
+            self._client = Server(url=_url, session=self._session,)
+            self._log_debug(f'Connected to CouchDb {_url}')
+            del _url
             if _username is not None and _password is not None:
-                self._client = Server(
-                    url=f'{_protocol}://{_username}:{_password}@{_host}:{_port}',
-                    session=self._session,)
-            else:
-                self._client = Server(url=f'{_protocol}://{_host}:{_port}', session=self._session,)
+                _token: Union[str, None] = self._client.login(_username, _password,)
+                if isinstance(_token, str):
+                    self._token = _token
+                else:
+                    self._token = None
+                del _token
         except ServerError:
             self._client = None
+        except Unauthorized:
+            self._client = None
         del _username, _password, _host, _port, _protocol
+        if self._client is None:
+            raise ConnectionException('Could not connect to CouchDB')
         _db_name: str = self._settings['couchdb.db']
-        _db: Database
+        _db: Union[Database, None] = None
         try:
-            _db = self._client[_db_name]
-        except Exception:
+            if _db_name in self._client:
+                _db = self._client[_db_name]
+            else:
+                _db = self._client.create(_db_name)
+        except ResourceNotFound:
             _db = self._client.create(_db_name)
+        self._db = _db
+
+    def _is_connected(self) -> bool:
+        if self._client is None:
+            return False
+        if self._token is None:
+            try:
+                return bool(self._client)
+            except Exception:
+                return False
+        return bool(self._client.verify_token(self._token))
+
+    def close(self) -> None:
+        if self._token is not None and self._client is not None:
+            self._client.logout(self._token)
+        self._db = None
+        self._token = None
+        self._client = None
         
--- src/tranquillity/interfaces/_settings.py
+++ src/tranquillity/interfaces/_settings.py
@@ -1,5 +1,42 @@
 from abc import ABC
+from typing import Any, Dict, List, Union
 
 class ISettings(ABC):
-    def __init__(self):
+    _data: Dict[str, str] = {}
+    _raise_on_missing: bool = True
+
+    def __init__(self,
+                 file: Union[str, None] = None,
+                 filetype: Union[str, None] = None,
+                 debug: bool = False,
+                 data: Union[Dict[str, Any], None] = None,
+                 defaults: Union[Dict[str, Any], None] = None,
+                 required_fields: Union[List[str], None] = None,
+                 raise_on_missing: bool = True,
+                 path: Union[str, None] = None,
+                 file_ext: Union[str, None] = None,
+                 read_only: bool = False,
+                 dump_if_not_exists: bool = True):
+        pass
+
+    def __getitem__(self, key: str) -> Union[str, None]:
+        return self.get(key=key, default=None)
+
+    def __setitem__(self, key: str, val: str) -> None:
+        pass
+
+    def get(self, key: str, default: Union[str, None] = None) -> Union[str, None]:
+        key = key.lower()
+        if key not in self._data.keys():
+            if default is not None:
+                if isinstance(default, str):
+                    return default
+                raise TypeError(f'default value must be of type str, got {type(default)}')
+            # TODO: Check if there is a global default
+            elif self._raise_on_missing:
+                raise KeyError(f'key "{key}" not found')
+            return None
+        return self._data[key]
+
+    def set(self, key: str, val: str) -> None:
         pass
