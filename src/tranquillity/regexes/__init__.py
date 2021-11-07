093d4bb90e26ca16e332eb8a14592f0008fddce7
Federico Pirani <freonius@gmail.com>
2021-11-07 17:38:08 +0100

    Regex

--- src/tranquillity/regexes/__init__.py
+++ src/tranquillity/regexes/__init__.py
@@ -1,22 +1,29 @@
+from enum import Enum, auto
 from re import compile, Pattern, RegexFlag, match, Match
 from typing import Dict, List, Union
 from dataclasses import dataclass
 
 
+class Protocol(Enum):
+    HTTP = auto()
+    HTTPS = auto()
+
+
 @dataclass
 class LinkParts:
     protocol: Union[str, None]
-    username: Union[str, None]
-    password: Union[str, None]
     domain: str
-    port: Union[int, None]
-    path: Union[List[str], None]
-    params: Union[Dict[str, str], None]
+    path: Union[List[str], None] = None
+    username: Union[str, None] = None
+    password: Union[str, None] = None
+    port: Union[int, None] = None
+    params: Union[Dict[str, str], None] = None
     anchor: Union[str, None] = None
+    verb: Union[str, None] = None
 
 
 LINK: Pattern[str] = compile(
-    r"^\s*((?:https?|wss|git|ssh)://)?([a-zA-Z_0-9-%]+(?:\:[a-zA-Z_0-9-%]+)?\@)?((?:[0-9\.])+|(?:(?:www\.)?[a-zA-Z_\.0-9-]+(?:\.[a-zA-Z]+)))(?:\:([0-9]+))?(/[a-zA-Z/_\.0-9-]*)?(\?[a-zA-Z_\.0-9-=%&]*)?$", flags=RegexFlag.M)
+    r"^\s*(?P<verb>(?:GET|POST|PUT|DELETE|PATCH)\s)?\s*(?P<protocol>(?:https?|wss|git|ssh|udp|file|udp|ftps?|tcp|ip|pop3)://)?(?P<credentials>[a-zA-Z_0-9-%]+(?:\:[a-zA-Z_0-9-%]+)?\@)?(?P<url>(?:/[a-zA-Z0-9_:-]+)|(?:[0-9\.])+|(?:(?:www\.)?[a-zA-Z_\.0-9-]+(?:\.[a-zA-Z]+)))(?:\:([0-9]+))?(?P<path>/[a-zA-Z/_\.0-9-]*)?(?P<params>\?[a-zA-Z_\.0-9-=%&]*)?(?P<anchor>\#[a-zA-Z/_\.0-9-]*)?$", flags=RegexFlag.M)
 
 
 def is_link(input: str) -> bool:
@@ -28,12 +35,13 @@ def parse_link(input: str) -> Union[LinkParts, None]:
     m: Union[Match, None] = match(LINK, input)
     if m is None:
         return None
-    _protocol: Union[str, None] = m.group(1)
-    _up: Union[str, None] = m.group(2)
-    _dom: Union[str, None] = m.group(3)
-    _prt: Union[str, None] = m.group(4)
-    _pth: Union[str, None] = m.group(5)
-    _prm: Union[str, None] = m.group(6)
+    _protocol: Union[str, None] = m.group('protocol')
+    _up: Union[str, None] = m.group('credentials')
+    _dom: Union[str, None] = m.group('url')
+    _prt: Union[str, None] = m.group('port')
+    _pth: Union[str, None] = m.group('path')
+    _prm: Union[str, None] = m.group('params')
+    _anchor: Union[str, None] = m.group('anchor')
     del m
     _u: Union[str, None] = None
     _p: Union[str, None] = None
@@ -56,5 +64,6 @@ def parse_link(input: str) -> Union[LinkParts, None]:
         path=[x for x in _pth.split('/') if x !=
               ''] if _pth is not None else None,
         params={k.split('=')[0]: k.split('=')[1] for k in [x.replace(
-            '?', '') for x in _prm.split('&')]} if _prm is not None else None
+            '?', '') for x in _prm.split('&')]} if _prm is not None else None,
+        anchor=_anchor
     )
