7bf764b4ad7173fd480688b43e83bbfaba362edb
Fred Pirani <freonius@gmail.com>
2022-01-05 21:11:39 +0100

    Query

--- src/python/query/tranquillity/query/__interface.py
+++ src/python/query/tranquillity/query/__interface.py
@@ -1,14 +1,15 @@
 # pylint: disable=invalid-name,missing-function-docstring,missing-class-docstring,missing-module-docstring,suppressed-message,locally-disabled
 
-from abc import ABC
-from re import Pattern
-from bson import ObjectId
 from datetime import date, datetime, time
-from typing import Any, Dict, Generic, List, Tuple, TypeVar, Union, Type, Iterable
+from typing import Any, Dict, List, Tuple, Union
 from enum import Enum, auto
 from dataclasses import dataclass
 
 # Enums
+
+
+class QueryFormatError(Exception):
+    pass
 
 
 class QueryComparison(Enum):
@@ -35,6 +36,9 @@ class QueryJoin(Enum):
     AndNot = auto()
     OrNot = auto()
     Init = auto()
+    Close = auto()
+    GroupInit = auto()
+    GroupClose = auto()
 
 
 class QueryAction(Enum):
@@ -45,129 +49,233 @@ class QueryAction(Enum):
     Delete = auto()
 
 
-T = TypeVar('T', bound=Any)
+class QueryType(Enum):
+    String = auto()
+    Int = auto()
+    Float = auto()
+    Num = auto()
+    Date = auto()
+    DateTime = auto()
+    Time = auto()
+    Uuid = auto()
+    Id = auto()
+    Object = auto()
+    Bool = auto()
+    List = auto()
+    MongoId = auto()
+
+
+class QWhereV:
+    _val: Union[None, str, date, datetime, time, int, float]
+
+    def __init__(self, val: Union[None, str, date, datetime, time, int, float]) -> None:
+        self._val = val
+        pass
+
+
+class QWhereVR:
+    _val: Tuple[Union[None, str, date, datetime, time, int, float], ...]
+
+    def __init__(self, min_val, max_val) -> None:
+        self._val = (min_val, max_val)
+        pass
 
 
 @dataclass
-class QueryCondition:
-    data_type: str
-    data_value: Union[
-        int,
-        str,
-        float,
-        date,
-        datetime,
-        time,
-        bool,
-        None,
-        Pattern,
-        ObjectId,
-        Tuple[int, int],
-        Tuple[float, float],
-        Tuple[date, date],
-        Tuple[datetime, datetime],
-        Tuple[time, time],
-        List[int],
-        List[float],
-        List[str],
-        List[date],
-        List[datetime],
-        List[time]]
+class WhereCondition:
+    join: QueryJoin
+    field: str
+    type: QueryType
     comparison: QueryComparison
-    comparison_join: QueryJoin
+    value: Union[QWhereV, QWhereVR]
 
 
-class QueryWhere:
-    _query: 'IQuery'
-
-    def __init__(self, query: 'IQuery') -> None:
-        self._query = query
-
-    def Where(self, field: str, comparison: str, value: str) -> 'QueryWhere':
-        return self
-
-    def And(self, field: str, comparison: str, value: str) -> 'QueryWhere':
-        return self
-
-    def Or(self, field: str, comparison: str, value: str) -> 'QueryWhere':
-        return self
-
-    def Group(self, conditions: List['QueryWhere']) -> 'QueryWhere':
-        return self
-
-    def NotGroup(self, conditions: List['QueryWhere']) -> 'QueryWhere':
-        return self
-
-    def AndGroup(self, conditions: List['QueryWhere']) -> 'QueryWhere':
-        return self
-
-    def OrGroup(self, conditions: List['QueryWhere']) -> 'QueryWhere':
-        return self
-
-    def AndNotGroup(self, conditions: List['QueryWhere']) -> 'QueryWhere':
-        return self
-
-    def OrNotGroup(self, conditions: List['QueryWhere']) -> 'QueryWhere':
-        return self
-
-    @property
-    def EndWhere(self) -> 'IQuery':
-        return self._query
-
-    @property
-    def All(self) -> 'IQuery':
-        return self._query
-
-
-class IQuery(ABC, Generic[T]):
+class Q_:
+    _where: List[Union[QueryJoin, str, QueryType,
+                       QWhereV, QWhereVR, QueryComparison]]
     _action: QueryAction
-    _t: Type[T]
-    _where: List[QueryCondition]
+    _table: str
+    _schema: Union[str, None]
+    _fields: Union[List[str], None]
+    _data: Union[Dict[str, Any], None]
 
-    def Where(self, where: QueryCondition) -> 'IQuery[T]':
-        self._where.append(where)
-        return self
-
-    def __init__(self, t: Type[T]) -> None:
-        self._t = t
+    def __init__(self, action: QueryAction) -> None:
+        self._action = action
         self._where = []
-        self._action = QueryAction.Select
 
-    @classmethod
-    def parse(cls, query: str, t: Union[Type[T], T]) -> 'IQuery[T]':
-        _query: 'IQuery[T]'
-        if isinstance(t, type):
-            _query = cls(t)
-        else:
-            _query = cls(type(t))
-        return _query
+    def _add_where_bit(self, bit: Union[QueryJoin, str, QueryType,
+                       QWhereV, QWhereVR, QueryComparison]) -> None:
+        self._where.append(bit)
+
+    @staticmethod
+    def Select(*fields: str, from_table: str, with_schema: Union[str, None] = None) -> 'QSelect':
+        return QSelect(Q_(QueryAction.Select))
+
+    @staticmethod
+    def Insert(data: Dict[str, Any], into_table: str, with_schema: Union[str, None] = None) -> None:
+        pass
+
+    @staticmethod
+    def Update(data: Dict[str, Any], from_table: str, with_schema: Union[str, None] = None) -> None:
+        pass
+
+    @staticmethod
+    def Delete(from_table: str, with_schema: Union[str, None] = None) -> None:
+        pass
+
+    @staticmethod
+    def Create(table: str, with_schema: Union[str, None] = None) -> None:
+        pass
+
+
+class QSelect:
+    _parent: Q_
+
+    def __init__(self, parent: Q_) -> None:
+        if not isinstance(parent, Q_):
+            raise QueryFormatError
+        self._parent = parent
 
     @property
-    def Select(self) -> QueryWhere:
-        self._action = QueryAction.Select
-        return QueryWhere(self)
-
-    def Update(self, obj: T, fields: Union[str, Iterable[str]]) -> QueryWhere:
-        self._action = QueryAction.Update
-        return QueryWhere(self)
-
-    def Insert(self, obj: T) -> 'IQuery[T]':
-        self._action = QueryAction.Insert
+    def And(self) -> 'QSelect':
+        self._parent._add_where_bit(QueryJoin.And)
         return self
 
     @property
-    def Delete(self) -> QueryWhere:
-        self._action = QueryAction.Delete
-        return QueryWhere(self)
-
-    def CreateTable(self) -> 'IQuery[T]':
-        self._action = QueryAction.Create
+    def Or(self) -> 'QSelect':
+        self._parent._add_where_bit(QueryJoin.Or)
         return self
 
-    def _validate(self) -> None:
-        if self._action is QueryAction.Create or self._action is QueryAction.Insert and len(self._where) > 0:
-            raise ValueError()
+    def Where(self, field: str) -> 'QWhereS':
+        self._parent._add_where_bit(QueryJoin.Init)
+        return QWhereS(self, field)
 
-    def convert(self) -> Tuple[Union[str, None], Union[Dict[str, Any], None]]:
-        self._validate()
-        return ('', {})
+    @property
+    def Group(self) -> 'QSelect':
+        self._parent._add_where_bit(QueryJoin.GroupInit)
+        return self
+
+    @property
+    def EndGroup(self) -> 'QSelect':
+        self._parent._add_where_bit(QueryJoin.GroupClose)
+        return self
+
+
+class QWhereS:
+    _parent: QSelect
+
+    def __init__(self, parent: QSelect, field: str) -> None:
+        self._parent = parent
+        self._parent._parent._add_where_bit(field)
+
+    @property
+    def WhichIsA(self) -> 'QWhereSAs':
+        return QWhereSAs(self)
+
+
+class QWhereSAs:
+    _parent: QWhereS
+
+    def __init__(self, parent: QWhereS) -> None:
+        self._parent = parent
+
+    @property
+    def Int(self) -> 'QWhereSIs':
+        self._parent._parent._parent._add_where_bit(QueryType.Int)
+        return QWhereSIs(self)
+
+    @property
+    def String(self) -> 'QWhereSIs':
+        self._parent._parent._parent._add_where_bit(QueryType.String)
+        return QWhereSIs(self)
+
+    @property
+    def Float(self) -> 'QWhereSIs':
+        self._parent._parent._parent._add_where_bit(QueryType.Float)
+        return QWhereSIs(self)
+
+    @property
+    def Date(self) -> 'QWhereSIs':
+        self._parent._parent._parent._add_where_bit(QueryType.Date)
+        return QWhereSIs(self)
+
+    @property
+    def DateTime(self) -> 'QWhereSIs':
+        self._parent._parent._parent._add_where_bit(QueryType.DateTime)
+        return QWhereSIs(self)
+
+    @property
+    def Time(self) -> 'QWhereSIs':
+        self._parent._parent._parent._add_where_bit(QueryType.Time)
+        return QWhereSIs(self)
+
+    @property
+    def MongoId(self) -> 'QWhereSIs':
+        self._parent._parent._parent._add_where_bit(QueryType.MongoId)
+        return QWhereSIs(self)
+
+    @property
+    def Bool(self) -> 'QWhereSIs':
+        self._parent._parent._parent._add_where_bit(QueryType.Bool)
+        return QWhereSIs(self)
+
+
+class QWhereSIs:
+    _parent: QWhereSAs
+
+    def __init__(self, parent: QWhereSAs) -> None:
+        self._parent = parent
+
+    def IsEqualTo(self, val) -> QSelect:
+        self._parent._parent._parent._parent._add_where_bit(QueryComparison.Eq)
+        self._parent._parent._parent._parent._add_where_bit(QWhereV(val))
+        self._parent._parent._parent._parent._add_where_bit(QueryJoin.Close)
+        return self._parent._parent._parent
+
+    def IsLike(self, val) -> QSelect:
+        self._parent._parent._parent._parent._add_where_bit(
+            QueryComparison.Like)
+        self._parent._parent._parent._parent._add_where_bit(QWhereV(val))
+        self._parent._parent._parent._parent._add_where_bit(QueryJoin.Close)
+        return self._parent._parent._parent
+
+    def IsNotEqualTo(self, val) -> QSelect:
+        self._parent._parent._parent._parent._add_where_bit(QueryComparison.Ne)
+        self._parent._parent._parent._parent._add_where_bit(QWhereV(val))
+        self._parent._parent._parent._parent._add_where_bit(QueryJoin.Close)
+        return self._parent._parent._parent
+
+    def IsGreaterThan(self, val) -> QSelect:
+        self._parent._parent._parent._parent._add_where_bit(QueryComparison.Gt)
+        self._parent._parent._parent._parent._add_where_bit(QWhereV(val))
+        self._parent._parent._parent._parent._add_where_bit(QueryJoin.Close)
+        return self._parent._parent._parent
+
+    def IsGreaterThanOrEqualTo(self, val) -> QSelect:
+        self._parent._parent._parent._parent._add_where_bit(
+            QueryComparison.Gte)
+        self._parent._parent._parent._parent._add_where_bit(QWhereV(val))
+        self._parent._parent._parent._parent._add_where_bit(QueryJoin.Close)
+        return self._parent._parent._parent
+
+    def IsNull(self) -> QSelect:
+        self._parent._parent._parent._parent._add_where_bit(
+            QueryComparison.IsNull)
+        self._parent._parent._parent._parent._add_where_bit(QWhereV(None))
+        self._parent._parent._parent._parent._add_where_bit(QueryJoin.Close)
+        return self._parent._parent._parent
+
+    def IsNotNull(self) -> QSelect:
+        self._parent._parent._parent._parent._add_where_bit(
+            QueryComparison.IsNotNull)
+        self._parent._parent._parent._parent._add_where_bit(QWhereV(None))
+        self._parent._parent._parent._parent._add_where_bit(QueryJoin.Close)
+        return self._parent._parent._parent
+
+    def IsBetween(self, min_val, max_val) -> QSelect:
+        self._parent._parent._parent._parent._add_where_bit(
+            QueryComparison.Between)
+        self._parent._parent._parent._parent._add_where_bit(
+            QWhereVR(min_val=min_val, max_val=max_val))
+        self._parent._parent._parent._parent._add_where_bit(QueryJoin.Close)
+        return self._parent._parent._parent
